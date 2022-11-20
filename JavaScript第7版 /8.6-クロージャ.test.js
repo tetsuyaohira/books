745613f4982226fbaa85f75b3d382a7b50ede028
tetsuyaohira <tetsuya.ohira@icloud.com>
2022-11-20 09:45:55 +0900

    8.6-クロージャ.test.js

--- JavaScript第7版 /8.6-クロージャ.test.js
+++ JavaScript第7版 /8.6-クロージャ.test.js
@@ -3,7 +3,7 @@ test('クロージャ', () => {
 
     function checkscope() {
         let scope = 'local scope'
-        function f() { return scope }
+        function f() { return scope } // このscopeは一番近い外部スコープの`local scope`を参照する
         return f();
     }
 
